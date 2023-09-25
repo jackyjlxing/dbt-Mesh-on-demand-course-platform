25097cf704bbab9203f74eefd397a8220b634e4b
Carol Ohms <carol.ohms@fishtownanalytics.com>
2023-09-25 21:11:34 +0000

    Removed seeds

--- models/staging/stg_locations.sql
+++ models/staging/stg_locations.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('ecom', 'raw_stores') }}
+    select * from {{ source('jaffle_shop', 'locations') }}
 
     -- if you generate a larger dataset,
     -- you can limit the timespan to the current time with the following line
