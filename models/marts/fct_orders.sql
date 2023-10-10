19e959e32561da524f88928559c95a04d1252372
Carol Ohms <carol.ohms@fishtownanalytics.com>
2023-10-10 00:48:02 +0000

    fully listed all columns

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -5,7 +5,16 @@ with orders as (
 
 final as (
     select 
-        orders.*,
+        order_id,
+        location_id,
+        customer_id,
+        order_total,
+        tax_paid,
+        ordered_at,
+        customer_name,
+        location_name,
+        tax_rate,
+        location_opened_at,
         date_part(month, ordered_at) as ordered_month,
         date_part(day, ordered_at) as ordered_day, 
         date_part(year, ordered_at) as ordered_year
